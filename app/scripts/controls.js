a8f1c853f101fa96421c18c7e1cae317b254ab33
viljamis <viljami.salminen@gmail.com>
2016-05-26 21:47:25 -0700

    Changes

--- app/scripts/controls.js
+++ app/scripts/controls.js
@@ -1,176 +1,182 @@
 $(document).ready(
 function() {
 
-	var type = document.getElementsByTagName("h1")[0];
+  var type = document.getElementsByTagName("h1")[0];
+  var html = document.documentElement;
 
-	$('input[type="range"]').on('input', function () {
+  $('input[type="range"]').on('input', function () {
             var percent = Math.ceil(((this.value - this.min) / (this.max - this.min)) * 100);
             console.log(this.min);
             $(this).css('background', '-webkit-linear-gradient(left, #419bf9 0%, #419bf9 ' + percent + '%, #B3B3B3 ' + percent + '%)');
         });
 
-	// hide controls and set heading images to closed
-	$('.group, #font, #otherfont').hide();
-	$('#controls h3').addClass("closed");
+  // hide controls and set heading images to closed
+  $('.group, #font, #otherfont').hide();
+  $('#controls h3').addClass("closed");
 
-	// show typeface selector by default
-	$('#legibility, #projectgroup').toggleClass("closed");
-	$('#legibility, #projectgroup').next(".group").show();
+  // show typeface selector by default
+  $('#legibility, #projectgroup').toggleClass("closed");
+  $('#legibility, #projectgroup').next(".group").show();
 
-	// show/hide control group
-	$('#controls h3').click(
-		function() {
-			$(this).toggleClass("closed");
-			$(this).next(".group").slideToggle(200);
-		}
-	);
-
-	// update changes
-	$("#inputForm").change(
-		function() {
-			refreshFeatures();
-		}
-	)
-	$("#reset").click(
-		function() {
-			window.location.reload();
-		}
-	)
-	//refreshFont();
-	//refreshFeatures();
-
-	var size = document.getElementById("textsize");
-	size.addEventListener("input", function () {
-		type.style.fontSize = size.value + "vw";
-	});
-
-	var distance = document.getElementById("distance");
-	distance.addEventListener("input", function () {
-		type.style.webkitTextStroke = distance.value / 20 + "px #fff";
-		type.style.webkitFilter = "blur(" + distance.value / 20 + "px)";
-	});
-
-	var contrast = document.getElementById("contrast");
-	contrast.addEventListener("input", function () {
-
-		type.style.opacity =  contrast.value / 100;
-	});
-
-	var overglow = document.getElementById("overglow");
-	overglow.addEventListener("input", function () {
-		type.style.webkitFilter = "blur(" + overglow.value / 3 + "px)";
-		type.style.webkitTextStroke = overglow.value / 2 + "px #222";
-	});
-})
-
-/*var defaultOff = [
-  'smcp', 'c2sc',
-  'lnum', 'onum', 'tnum', 'pnum',
-  'frac', 'afrc',
-  'sups', 'subs',
-  'zero', 'nalt',
-  'kern',
-  'liga', 'dlig', 'hlig', 'clig',
-  'swsh', 'calt', 'hist', 'salt',
-  'ss01', 'ss02', 'ss03', 'ss04', 'ss05','ss06', 'ss07', 'ss08', 'ss09'
-];
-
-var defaultOn = [];
-
-function refreshFeatures() {
-
-  var mfeatures = "";
-  var wfeatures = "";
-  var f;
-  for (f in defaultOn) {
-    if (!document.getElementById(defaultOn[f]).checked) { wfeatures += "&quot;" + defaultOn[f] + "&quot; 0, "; }
-  }
-  for (f in defaultOff) {
-    if (document.getElementById(defaultOff[f]).checked) { wfeatures += "&quot;" + defaultOff[f] + "&quot; 1, "; }
-  }
-  wfeatures = wfeatures.substring(0, wfeatures.length - 2);
-  document.getElementById("mozfeatures13").innerHTML = wfeatures;
-  document.getElementById("msfeatures").innerHTML = wfeatures;
-  document.getElementById("ofeatures").innerHTML = wfeatures;
-  document.getElementById("webkitfeatures").innerHTML = wfeatures;
-  document.getElementById("w3cfeatures").innerHTML = wfeatures;
-
-  refreshSample();
-
-};
-
-function refreshSample() {
-
-  var sample = document.getElementById("sampleText");
-
-  italic = document.getElementById("italic").checked ? "italic" : "";
-  italicfamily = italic ? "'" + document.getElementById("font").value+" Italic', " : "";
-  fontFamily = italicfamily + "'"+document.getElementById("font").value+"'";
-  sample.style.fontFamily = fontFamily + ", sans-serif";
-  sample.style.fontStyle = italic;
-
-  var wfeatures = document.getElementById("webkitfeatures").innerHTML;
-
-  if ("MozFontFeatureSettings" in sample.style) {
-    // first, reset the property to normal
-    sample.style.MozFontFeatureSettings = "normal";
-
-    // old Firefox syntax
-    var mfeatures = document.getElementById("mozfeatures").innerHTML;
-    sample.style.MozFontFeatureSettings = "'" + mfeatures + "'";
-
-    // if that failed setting will be "normal", use standard syntax
-    if (sample.style.MozFontFeatureSettings == "normal") {
-      sample.style.MozFontFeatureSettings = wfeatures;
+  // show/hide control group
+  $('#controls h3').click(
+    function() {
+      $(this).toggleClass("closed");
+      $(this).next(".group").slideToggle(200);
     }
-  }
+  );
 
-  document.getElementById("w3cfeatures").innerHTML = wfeatures;
-  sample.style.msFontFeatureSettings = wfeatures;
-  sample.style.oFontFeatureSettings = wfeatures;
-  sample.style.webkitFontFeatureSettings = wfeatures;
-  sample.style.fontFeatureSettings = wfeatures;
-};
+  // update changes
+  $("#inputForm").change(
+    function() {
+      refreshFeatures();
+    }
+  )
+  $("#reset").click(
+    function() {
+      window.location.reload();
+    }
+  )
+
+  refreshFeatures();
+	refreshFont();
+
+  var size = document.getElementById("textsize");
+  var distance = document.getElementById("distance");
+  var contrast = document.getElementById("contrast");
+  var overglow = document.getElementById("overglow");
+	var pixelation = document.getElementById("pixelation");
+	function getSize() {
+		var style = window.getComputedStyle(type, null).getPropertyValue('font-size') || 0;
+		return parseFloat(style);
+	}
+	var initialSize = getSize();
+	var once = false;
+
+  size.addEventListener("input", function () {
+		if (size.value > 0) {
+      html.classList.add("size");
+    } else {
+      html.classList.remove("size");
+    }
+		once = false;
+		type.style.fontSize = size.value + "vw";
+		type.style.transform = "translateY(-50%) translateX(-50%) translateZ(0)";
+		html.classList.remove("pixelation");
+		pixelation.value = 0;
+		pixelation.style.background = "-webkit-linear-gradient(left, rgb(65, 155, 249) 0%, rgb(65, 155, 249) 0%, rgb(179, 179, 179) 0%)";
+		once = false;
+
+  });
+
+  distance.addEventListener("input", function () {
+    if (distance.value > 0) {
+      html.classList.add("distance");
+    } else {
+      html.classList.remove("distance");
+    }
+    if (html.classList.contains("overglow")) {
+      type.style.webkitFilter = "blur(" + ((distance.value / 20) + (overglow.value / 3)) + "px)";
+    } else {
+      type.style.webkitTextStroke = distance.value / 20 + "px #fff";
+      type.style.webkitFilter = "blur(" + distance.value / 20 + "px)";
+    }
+  });
+
+  contrast.addEventListener("input", function () {
+    type.style.opacity =  contrast.value / 100;
+  });
+
+  overglow.addEventListener("input", function () {
+    if (overglow.value > 0) {
+      html.classList.add("overglow");
+    } else {
+      html.classList.remove("overglow");
+    }
+    if (html.classList.contains("distance")) {
+      type.style.webkitTextStroke = ((distance.value / 20) + (overglow.value / 2)) + "px #222";
+    } else {
+      type.style.webkitFilter = "blur(" + overglow.value / 8 + "px)";
+      type.style.webkitTextStroke = overglow.value / 2 + "px #222";
+    }
+  });
+
+	pixelation.addEventListener("input", function () {
+		if (!once) {
+			initialSize = getSize();
+			once = true;
+		}
+		if (pixelation.value > 0) {
+				html.classList.add("pixelation");
+				type.style.transform = "translateY(-50%) translateX(-50%) translateZ(0) scale(" + pixelation.value + ")";
+				type.style.fontSize = initialSize / pixelation.value + "px";
+    } else {
+			type.style.transform = "translateY(-50%) translateX(-50%)";
+      html.classList.remove("pixelation");
+			type.style.fontSize = initialSize + "px";
+			once = false;
+    }
+  });
+
+	var defaultOff = [
+	  'smcp', 'c2sc',
+	  'lnum', 'onum', 'tnum', 'pnum',
+	  'frac', 'afrc',
+	  'sups', 'subs',
+	  'zero', 'nalt',
+	  'kern',
+	  'liga', 'dlig', 'hlig', 'clig',
+	  'swsh', 'calt', 'hist', 'salt',
+	  'ss01', 'ss02', 'ss03', 'ss04', 'ss05','ss06', 'ss07', 'ss08', 'ss09'
+	];
+
+	var defaultOn = [];
+
+	function refreshFeatures() {
+
+	  var mfeatures = "";
+	  var wfeatures = "";
+	  var f;
+	  for (f in defaultOn) {
+	    if (!document.getElementById(defaultOn[f]).checked) { wfeatures += "'" + defaultOn[f] + "' 0, "; }
+	  }
+	  for (f in defaultOff) {
+	    if (document.getElementById(defaultOff[f]).checked) { wfeatures += "'" + defaultOff[f] + "' 1, "; }
+	  }
+
+		if ("MozFontFeatureSettings" in type.style) {
+	    // first, reset the property to normal
+	    type.style.MozFontFeatureSettings = "normal";
+
+	    // old Firefox syntax
+	    type.style.MozFontFeatureSettings = "'" + wfeatures + "'";
+
+	    // if that failed setting will be "normal", use standard syntax
+	    if (type.style.MozFontFeatureSettings == "normal") {
+	      type.style.MozFontFeatureSettings = wfeatures;
+	    }
+	  }
+
+	  wfeatures = wfeatures.substring(0, wfeatures.length - 2);
+		type.style.msFontFeatureSettings = "'" + wfeatures + "'";
+	  type.style.oFontFeatureSettings = "'" + wfeatures + "'";
+	  type.style.webkitFontFeatureSettings = wfeatures;
+	  type.style.fontFeatureSettings = wfeatures;
+	};
+
+});
 
 function refreshFont() {
-    var typefaceSelect = document.getElementById("typeface");
-    if ((typefaceSelect.selectedIndex) == 0) {
-        $('#otherfont').show();
-    } else {
-        $('#otherfont').hide();
-        document.getElementById("font").value = typefaceSelect.value;
-    }
+		var typefaceSelect = document.getElementById("typeface");
+		if (typefaceSelect.selectedIndex == 0) {
+			refreshOther();
+			$('#otherfont').show();
+		} else {
+			$('#otherfont').hide();
+			document.getElementsByTagName("h1")[0].style.fontFamily = typefaceSelect.value;
+		}
 }
 
 function refreshOther() {
-    document.getElementById("font").value = document.getElementById("otherfont").value;
-    refreshSample();
+		document.getElementsByTagName("h1")[0].style.fontFamily = document.getElementById("otherfont").value;
 }
-
-function getFamilies() {
-	var all_styles = document.getElementsByTagName('style');
-	var fontdeck_style = all_styles[0];
-	var style_rules = fontdeck_style.childNodes[0].nodeValue;
-	var style_rules_ar = style_rules.split("font-family:'");
-	var families = new Array();
-
-	for (var i = 1; i < style_rules_ar.length; i++) {
-		families[i-1] = style_rules_ar[i].split("'")[0];
-    }
-
-    return families;
-}
-
-function initFamilies() {
-	var families = getFamilies();
-	var typeface_select = document.getElementById("typeface");
-	for (var i = 0; i < families.length; i++) {
-		var option = document.createElement("option");
-		var family = document.createTextNode(families[i]);
-		option.appendChild(family);
-		option.setAttribute("value", families[i]);
-		typeface_select.appendChild(option);
-    }
-}
-
-window.onload = initFamilies;*/
